d2f486af4b160985531d553f9c25e39e43ed5724
Bruce M. Axtens <bugmagnet@outlook.com>
2021-06-14 09:38:22 +0800

    missing folder contains lost TwoFerTest.vb

---
+++ missing/TwoFerTests.vb
@@ -0,0 +1,15 @@
+﻿Imports Xunit
+Public Class TwoFerTests
+    <Fact>
+    Public Sub No_name_given()
+        Assert.Equal("One for you, one for me.", TwoFer.Speak())
+    End Sub
+    <Fact>
+    Public Sub A_name_given()
+        Assert.Equal("One for Alice, one for me.", TwoFer.Speak("Alice"))
+    End Sub
+    <Fact>
+    Public Sub Another_name_given()
+        Assert.Equal("One for Bob, one for me.", TwoFer.Speak("Bob"))
+    End Sub
+End Class
